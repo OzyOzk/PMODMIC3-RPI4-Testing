dfcf7e2ef22e7043f038212d371e1f602d659dc0
oozkilinc <oguzhan.ozkilinc@arup.com>
2020-12-07 14:26:38 +0000

    PMT - 0 - Removed comments and code from sample code and previous testing

--- pmod_mic3.c
+++ pmod_mic3.c
@@ -3,11 +3,7 @@
 #include <pigpio.h>
 #include <unistd.h>
 
-#define CLOCK_SPEED 500000
-
-/*
-   gcc -pthread -o spi-speed spi-speed.c -lpigpio
-*/
+#define BAUD_RATE 500000
 
 struct timespec ts;
 
@@ -17,7 +13,7 @@ double pmod_mic3(double* in_array, int size){
   ts.tv_sec = 0;
   double start, delta;
   double analog = 0;
-  int h = spiOpen(0, CLOCK_SPEED, 0);
+  int h = spiOpen(0, BAUD_RATE, 0);
   if (h < 0) return 1;
   
   /*bits to send*/
@@ -27,21 +23,15 @@ double pmod_mic3(double* in_array, int size){
   unsigned char buf[2] = {0, 0};
   
   start = time_time();
-  
-  //double tmp_start, tmp_delta;
+
 
   for (int i=0; i<size; i++){
-    //tmp_start = time_time();
     spiXfer(h, snd, buf, 2);
     analog = buf[0] << 8 | buf[1];
     in_array[i] = analog;
-    //nanosleep(&ts, NULL);
-    //tmp_delta = time_time() - tmp_start;
-    //printf("%f\n", tmp_delta);
   }
   
   delta = time_time() - start;
   spiClose(h);
-  //printf("%f\n", delta);
   return delta;
 }
